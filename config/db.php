ea3dcad4ad4c2728d2bb30bb4183998973a7a82e
Valentin Stepanenko <catomik13@gmail.com>
2017-10-10 09:21:20 +0300

    Ignore configs

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=time_tracker_db',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=timetracker',
+    'username' => 'zapleo',
+    'password' => '1q2w3e4r',
     'charset' => 'utf8',
 ];
