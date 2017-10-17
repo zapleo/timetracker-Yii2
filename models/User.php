76f34599d55f1d792938531f3044eae216831fee
Valentin Stepanenko <catomik13@gmail.com>
2017-10-17 14:36:15 +0300

    Fix is admin

--- models/User.php
+++ models/User.php
@@ -168,6 +168,6 @@ class User extends ActiveRecord implements IdentityInterface
 
     public function isAdmin()
     {
-        return is_null($this)?null:$this->rights;
+        return $this->rights;
     }
 }
--- views/layouts/js_options.php
+++ views/layouts/js_options.php
@@ -2,9 +2,10 @@
 /**
  * @var $this \yii\web\View
  */
+$admin = is_null(Yii::$app->user->identity)?false:Yii::$app->user->identity->isAdmin();
 ?>
 <script type="text/javascript">
     var base_url = <?= json_encode(Yii::$app->params['base_url']) ?>;
     var user_id = <?= Yii::$app->user->id ?>;
-    var is_admin = <?= Yii::$app->user->identity->isAdmin() ?>;
+    var is_admin = <?=$admin?>;
 </script>
