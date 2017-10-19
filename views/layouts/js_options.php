2e93248ee0e61aff600c727c93722d6d49e896c5
Valentin Smagluk <1valeks7@gmail.com>
2017-10-19 08:12:37 +0300

    TimeTracker update

--- views/layouts/js_options.php
+++ views/layouts/js_options.php
@@ -2,10 +2,12 @@
 /**
  * @var $this \yii\web\View
  */
-$admin = is_null(Yii::$app->user->identity)?false:Yii::$app->user->identity->isAdmin();
+
+$user_id = is_null(Yii::$app->user->id) ? false : Yii::$app->user->id;
+$admin = is_null(Yii::$app->user->identity) ? false : Yii::$app->user->identity->isAdmin();
 ?>
 <script type="text/javascript">
     var base_url = <?= json_encode(Yii::$app->params['base_url']) ?>;
-    var user_id = <?= Yii::$app->user->id ?>;
-    var is_admin = <?=$admin?>;
+    var user_id = <?= $user_id ?>;
+    var is_admin = <?= $admin ?>;
 </script>
